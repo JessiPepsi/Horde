2ac0b691ed3bd3ff0df29dbe0a438285860f6cbb
JessiPepsi <jonte1997@gmail.com>
2024-12-07 03:10:49 +0100

    NPCZ Content #1

---
+++ gamemode/arccw/attachments/bo1_skorpion_stock_folded.lua
@@ -0,0 +1,20 @@
+if not ArcCWInstalled then return end
+
+AddCSLuaFile()
+
+local att = {}
+
+att.PrintName = "Folded Stock"
+att.AbbrevName = "Folded Stock"
+att.Icon = Material("entities/att/npczv1grey.png", "smooth mips")
+att.Description = "Folded wire stock"
+att.Slot = "skorpion_misc"
+att.AutoStats = true
+att.ActivateElements = {"folded_stock"}
+
+att.Free = true
+
+
+att.SortOrder = 1
+att.Ignore = false
+ArcCW.LoadAttachmentType(att, "bo1_skorpion_stock_folded")
